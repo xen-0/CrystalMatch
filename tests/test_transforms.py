2e4dba54d856357e50e77f0b4a9c32be5508cd8b
Kris Ward <kristopher.ward@tessella.com>
2016-01-26 11:16:38 +0000

    File for transform tests (no implementation yet)

---
+++ tests/test_transforms.py
@@ -0,0 +1,11 @@
+import unittest
+import nose
+
+class TestReedSolo(unittest.TestCase):
+
+    def test_number_1(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
