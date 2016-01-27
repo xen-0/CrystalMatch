b686da59688fb17095531f65f42c1d7bbc8d015e
Kris Ward <kristopher.ward@tessella.com>
2016-01-27 11:17:14 +0000

    Created GUI for selecting images and performing matches

--- dls_imagematch/gui.py
+++ dls_imagematch/gui.py
@@ -1,32 +1,47 @@
 import sys
 import os
-from PyQt4 import QtGui, QtCore
+from functools import partial, reduce
+import cv2
 
-from PyQt4.QtGui import (QFileSystemModel, QTreeView)
-from PyQt4.QtCore import QDir, Qt
+from PyQt4 import QtGui
+from PyQt4.QtGui import (QWidget, QFileSystemModel, QTreeView, QLabel, QPushButton,
+                         QMainWindow, QIcon, QHBoxLayout, QVBoxLayout, QPixmap, QApplication, QAction)
+from PyQt4.QtCore import (Qt, SIGNAL)
+
+from dls_imagematch import (find_consensus_tr, find_tr, apply_tr, get_size, grain_extract)
 
 
 INPUT_DIR_ROOT = "../test-images/old/"
 
 
-class ImageMatcher(QtGui.QMainWindow):
+class ImageMatcher(QMainWindow):
     def __init__(self):
         super(ImageMatcher, self).__init__()
 
         self._fileTreeView = None
         self.model = None
+        self._selection_A = None
+        self._selection_B = None
+
+        self._main_frame = None
+        self._selection_A_frame = None
+        self._selection_A_label = None
+        self._selection_B_frame = None
+        self._selection_B_label = None
 
         self._init_ui()
 
     def _init_ui(self):
-        self.setGeometry(100, 100, 1020, 650)
+        self.setGeometry(100, 100, 1500, 650)
         self.setWindowTitle('Diamond VMXi Image Matching')
-        self.setWindowIcon(QtGui.QIcon('web.png'))
+        self.setWindowIcon(QIcon('web.png'))
 
+        self.init_menu_bar()
+
+        # Create view and model for file explorer - selecting an image displays it in the main frame
         self.model = QFileSystemModel()
-        # You can setRootPath to any path.
         self.model.setRootPath(INPUT_DIR_ROOT)
-        # List of views.
+
         self._fileTreeView = QTreeView()
         self._fileTreeView.setModel(self.model)
         self._fileTreeView.setRootIndex(self.model.index(INPUT_DIR_ROOT))
@@ -39,51 +54,225 @@ class ImageMatcher(QtGui.QMainWindow):
         self._fileTreeView.hideColumn(2)
 
         self._fileTreeView.connect(self._fileTreeView.selectionModel(),
-                                   QtCore.SIGNAL("selectionChanged(QItemSelection, QItemSelection)"),
+                                   SIGNAL("selectionChanged(QItemSelection, QItemSelection)"),
                                    self.new_file_selected)
 
-        # Image frame - displays image of the currently selected image
-        self._imageFrame = QtGui.QLabel()
-        self._imageFrame.setStyleSheet("background-color: black; color: red; font-size: 30pt; text-align: center")
-        self._imageFrame.setFixedWidth(600)
-        self._imageFrame.setFixedHeight(600)
+        # Image frame - displays the image currently selected in the file tree
+        self._main_frame = QLabel()
+        self._main_frame.setStyleSheet("background-color: black; color: red; font-size: 30pt; text-align: center")
+        self._main_frame.setFixedWidth(600)
+        self._main_frame.setFixedHeight(600)
+
+        # Selection buttons - make selection of currently displayed image as A or B
+        self._select_A_button = QPushButton("Select A")
+        self._select_A_button.clicked.connect(self._select_A_pushed)
+        self._select_B_button = QPushButton("Select B")
+        self._select_B_button.clicked.connect(self._select_B_pushed)
+
+        # Selection filename - displays filename of selected images (A and B)
+        self._selection_A_label = QLabel()
+        self._selection_A_label.setFixedWidth(300)
+        self._selection_A_label.setText("No Image Selected")
+
+        self._selection_B_label = QLabel()
+        self._selection_B_label.setFixedWidth(300)
+        self._selection_B_label.setText("No Image Selected")
+
+        # Selection Image Frames - displays smaller versions of currently selected images (A and B)
+        self._selection_A_frame = QLabel()
+        self._selection_A_frame.setStyleSheet("background-color: black; color: red; font-size: 20pt; text-align: center")
+        self._selection_A_frame.setFixedWidth(250)
+        self._selection_A_frame.setFixedHeight(250)
+        self._selection_A_frame.setText("No Image Selectede")
+        self._selection_A_frame.setAlignment(Qt.AlignCenter)
+
+        self._selection_B_frame = QLabel()
+        self._selection_B_frame.setStyleSheet("background-color: black; color: red; font-size: 20pt; text-align: center")
+        self._selection_B_frame.setFixedWidth(250)
+        self._selection_B_frame.setFixedHeight(250)
+        self._selection_B_frame.setText("No Image Selected")
+        self._selection_B_frame.setAlignment(Qt.AlignCenter)
 
         # Create layout
-        hbox = QtGui.QHBoxLayout()
+        hbox = QHBoxLayout()
         hbox.setSpacing(10)
         hbox.addWidget(self._fileTreeView)
-        hbox.addWidget(self._imageFrame)
+        hbox.addWidget(self._main_frame)
+
+        hbox_A = QHBoxLayout()
+        hbox_A.addWidget(self._select_A_button)
+        hbox_A.addWidget(self._selection_A_label)
+        vbox_A = QVBoxLayout()
+        vbox_A.addLayout(hbox_A)
+        vbox_A.addWidget(self._selection_A_frame)
+        vbox_A.addStretch(1)
+
+        hbox_B = QHBoxLayout()
+        hbox_B.addWidget(self._select_B_button)
+        hbox_B.addWidget(self._selection_B_label)
+        vbox_B = QVBoxLayout()
+        vbox_B.addLayout(hbox_B)
+        vbox_B.addWidget(self._selection_B_frame)
+        vbox_B.addStretch(1)
+
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(vbox_A)
+        vbox.addLayout(vbox_B)
+        #vbox.addStretch(1)
+
+        hbox.addLayout(vbox)
         hbox.addStretch(1)
 
-        main_widget = QtGui.QWidget()
+        main_widget = QWidget()
         main_widget.setLayout(hbox)
         self.setCentralWidget(main_widget)
         self.show()
 
+    def init_menu_bar(self):
+        """Create and populate the menu bar.
+        """
+        # Exit Application
+        exit_action = QAction(QIcon('exit.png'), '&Exit', self)
+        exit_action.setShortcut('Ctrl+Q')
+        exit_action.setStatusTip('Exit application')
+        exit_action.triggered.connect(QtGui.qApp.quit)
+
+        # Match Action
+        match_action = QAction(QIcon('exit.png'), '&Perform Match', self)
+        match_action.setShortcut('Ctrl+M')
+        match_action.setStatusTip('Perform Match')
+        match_action.triggered.connect(self._perform_image_matching)
+
+        # Create menu bar
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('&File')
+        file_menu.addAction(exit_action)
+
+        match_menu = menu_bar.addMenu('&Match')
+        match_menu.addAction(match_action)
+
     def new_file_selected(self, selected, deselected):
-        indexes = selected.indexes()
-        index = indexes[0]
-        filepath = self.model.filePath(index)
-        print(filepath)
+        filepath = self._get_selected_filepath()
+        self._display_image(self._main_frame, filepath)
+        self._set_filename_label(self._selection_B_label, filepath)
 
-        self._display_image(filepath)
+    def _select_A_pushed(self):
+        filepath = self._get_selected_filepath()
+        self._display_image(self._selection_A_frame, filepath)
+        self._set_filename_label(self._selection_A_label, filepath)
+        self._selection_A = filepath
 
-    def _display_image(self, filename):
-        self._imageFrame.clear()
-        self._imageFrame.setAlignment(QtCore.Qt.AlignCenter)
+    def _select_B_pushed(self):
+        filepath = self._get_selected_filepath()
+        self._display_image(self._selection_B_frame, filepath)
+        self._set_filename_label(self._selection_B_label, filepath)
+        self._selection_B = filepath
+
+    def _get_selected_filepath(self):
+        indexes = self._fileTreeView.selectedIndexes()
+        if indexes:
+            index = indexes[0]
+            filepath = self.model.filePath(index)
+            return filepath
+        else:
+            return None
+
+    def _display_image(self, frame, filename):
+        frame.clear()
+        frame.setAlignment(Qt.AlignCenter)
 
         if filename is None:
-            self._imageFrame.setText("No Scan Selected")
+            frame.setText("No Image Selected")
         elif os.path.isfile(filename):
-            pixmap = QtGui.QPixmap(filename)
-            self._imageFrame.setPixmap(pixmap.scaled(self._imageFrame.size(),
-                                                     QtCore.Qt.KeepAspectRatio, QtCore.Qt.SmoothTransformation))
+            pixmap = QPixmap(filename)
+            frame.setPixmap(pixmap.scaled(frame.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
         else:
-            self._imageFrame.setText("Image Not Found")
+            frame.setText("Image Not Found")
+
+    def _set_filename_label(self, label, filepath):
+        if filepath and filepath != "":
+            label.setText(filepath.split('/')[-1])
+        else:
+            label.setText("No Image Selected")
+
+    def _perform_image_matching(self):
+
+        if not self._selection_A or not self._selection_B:
+            return
+        if self._selection_A == self._selection_B:
+            return
+
+        PROFILING = False
+        DISPLAY_PROGRESS = True
+        DISPLAY_RESULTS = False
+        OUTPUT_DIRECTORY = None
+        TRANSLATIONS_OUTPUT_FILE = None
+        CONSENSUS = False  # If True, cannot display progress.
+        N_PROCESSES = 8  # How many CPU cores to use (sort of).
+        TEST_SET = 'B'  # Test set B is more interesting than A.
+        CROP_AMOUNTS = [0.12]*4
+
+        # Real image dimensions, in microns... of the reference?
+        # (These dimensions are for test set A.)
+        image_physical_width, image_physical_height = map(float, (2498, 2004))
+
+        ref_file = self._selection_A
+        trans_file = self._selection_B
+
+        print(ref_file, trans_file)
+
+        ref, trans = map(cv2.imread, (ref_file, trans_file))
+        ref, trans = map(make_gray, (ref, trans))
+
+        find_tr_fn = partial(find_consensus_tr, N_PROCESSES) if CONSENSUS else find_tr
+
+        net_transform = find_tr_fn(
+            ref, trans,
+            debug=DISPLAY_PROGRESS, crop_amounts=CROP_AMOUNTS)
+
+        image_width, image_height = get_size(ref)
+        t = net_transform((image_width, image_height))
+
+        delta_x = -t[0, 2]*image_physical_width/image_width
+        delta_y = +t[1, 2]*image_physical_height/image_height
+
+        print('---\ndelta_x is', delta_x, 'µm; delta_y is', delta_y, 'µm\n---')
+        print(t)
+        print('===')
+
+        '''
+        if TRANSLATIONS_OUTPUT_FILE is not None:
+            with open(TRANSLATIONS_OUTPUT_FILE, 'a') as f:
+                f.write(table_line((str(samp_num), str(trans_num),
+                                    str(-t[0, 2]), str(t[1, 2]),
+                                    str(delta_x), str(delta_y))))
+
+        if DISPLAY_RESULTS:
+            cv2.imshow(
+                'sample'+str(samp_num)+' comparison'+str(trans_num),
+                cv2.absdiff(ref, apply_tr(net_transform, trans)))
+            cv2.waitKey(0)
+
+        if OUTPUT_DIRECTORY is not None:
+            cv2.imwrite(
+                path.join(
+                    OUTPUT_DIRECTORY,
+                    str(samp_num)+'_'+str(ref_num)+'_'+str(trans_num)+'.jpg'),
+                grain_extract(ref, apply_tr(net_transform, trans)))
+        '''
+
+
+def make_gray(img):
+    if len(img.shape) in (3, 4):
+        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    else:
+        return img
+
 
 
 def main():
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     ex = ImageMatcher()
     sys.exit(app.exec_())
 
