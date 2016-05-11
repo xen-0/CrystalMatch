fd2f839167dd29def8c3a10f68dd696ef8aaa35a
Kris Ward <kristopher.ward@tessella.com>
2016-05-11 13:03:42 +0100

    Renamed some gui files

--- dls_imagematch/gui/__init__.py
+++ dls_imagematch/gui/__init__.py
@@ -4,8 +4,8 @@ from .image_select import ImageSelector
 from .well_select import WellSelector
 from .well_select2 import WellSelector2
 from .image_frame import ImageFrame
-from .rmatch_control import RegionMatchControl
-from .fmatch_control import FeatureMatchControl
-from .cmatch_control import ConsensusMatchControl
+from .match_region_control import RegionMatchControl
+from .match_feature_control import FeatureMatchControl
+from .match_consensus_control import ConsensusMatchControl
 
 
