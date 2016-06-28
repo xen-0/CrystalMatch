cd21851347c2bff2c9c370bcdd0b33fa254a3361
Kris Ward <kristopher.ward@tessella.com>
2016-06-28 09:55:50 +0100

    Split translation only functionality from transformation class into new Translation class

---
+++ dls_imagematch/match/translation.py
@@ -0,0 +1,29 @@
+from dls_imagematch.util import Image
+
+
+class Translation:
+    def __init__(self, translation):
+        self._translation = translation
+
+    def translation(self):
+        return self._translation
+
+    def transform_image(self, image, output_size):
+        offset = self._translation
+        warped = Image.blank(output_size[0], output_size[1], image.channels)
+        warped.paste(image, offset)
+        return warped
+
+    def inverse_transform_image(self, image, output_size):
+        offset = - self._translation
+        warped = Image.blank(output_size[0], output_size[1], image.channels)
+        warped.paste(image, offset)
+        return warped
+
+    def transform_points(self, points):
+        transformed = [p + self._translation for p in points]
+        return transformed
+
+    def inverse_transform_points(self, points):
+        transformed = [p - self._translation for p in points]
+        return transformed
