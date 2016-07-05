65f62ce4c392440d38c2dc3d5be5632f539c163a
Kris Ward <kristopher.ward@tessella.com>
2016-07-05 14:46:58 +0100

    Refactoring

--- dls_imagematch/gui/crystal/single_match_dialog.py
+++ dls_imagematch/gui/crystal/single_match_dialog.py
@@ -45,32 +45,14 @@ class SingleCrystalDialog(QDialog):
         btn_select_point = QPushButton("Select Crystal")
         btn_select_point.clicked.connect(self._fn_select_points)
 
-        lbl_region = QLabel("Region Size")
-        lbl_region.setFixedWidth(LABEL_WIDTH)
-        region_size = self._config.region_size.value()
-        self._txt_region_size = QLineEdit(str(region_size))
-        hbox_region = QHBoxLayout()
-        hbox_region.addWidget(lbl_region)
-        hbox_region.addWidget(self._txt_region_size)
-        hbox_region.addStretch()
+        region_size = str(self._config.region_size.value())
+        self._txt_region_size, hbox_region = self._ui_txt_box("Region Size", LABEL_WIDTH, region_size)
 
-        lbl_width = QLabel("Search Width")
-        lbl_width.setFixedWidth(LABEL_WIDTH)
-        search_width = self._config.search_width.value()
-        self._txt_search_width = QLineEdit(str(search_width))
-        hbox_search_w = QHBoxLayout()
-        hbox_search_w.addWidget(lbl_width)
-        hbox_search_w.addWidget(self._txt_search_width)
-        hbox_search_w.addStretch()
+        search_width = str(self._config.search_width.value())
+        self._txt_search_width, hbox_search_w = self._ui_txt_box("Search Width", LABEL_WIDTH, search_width)
 
-        lbl_height = QLabel("Search Height")
-        lbl_height.setFixedWidth(LABEL_WIDTH)
-        search_height = self._config.search_height.value()
-        self._txt_search_height = QLineEdit(str(search_height))
-        hbox_search_h = QHBoxLayout()
-        hbox_search_h.addWidget(lbl_height)
-        hbox_search_h.addWidget(self._txt_search_height)
-        hbox_search_h.addStretch()
+        search_height = str(self._config.search_height.value())
+        self._txt_search_height, hbox_search_h = self._ui_txt_box("Search Height", LABEL_WIDTH, search_height)
 
         vbox = QVBoxLayout()
         vbox.addLayout(hbox_region)
@@ -81,6 +63,18 @@ class SingleCrystalDialog(QDialog):
 
         grp_box.setLayout(vbox)
         return grp_box
+
+    def _ui_txt_box(self, label, label_width, initial_value):
+        lbl = QLabel(label)
+        lbl.setFixedWidth(label_width)
+        txt = QLineEdit(initial_value)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(lbl)
+        hbox.addWidget(txt)
+        hbox.addStretch()
+
+        return txt, hbox
 
     def _fn_select_points(self):
         max_points = 1
