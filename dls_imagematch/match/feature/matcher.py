bdc53c269bed18becad9041aebf00f8f9da6a711
Kris Ward <kristopher.ward@tessella.com>
2016-07-06 09:46:26 +0100

    Moved BoundedFeatureMatcher into its own file

--- dls_imagematch/match/feature/matcher.py
+++ dls_imagematch/match/feature/matcher.py
@@ -13,9 +13,9 @@ from .detector import FeatureDetector
 class FeatureMatcher:
     """ Use feature matching to compare two images and align the second on the first.
 
-    Performs a feature matching operation between the two selected images (or image regions if
-    specified. The resulting transformation can be applied to a point in Image/Region 1 coordinates
-    to map it to its matching point in Image 2 coordinates.
+    Performs a feature matching operation between the two selected images. The resulting
+    transformation can be applied to a point in Image/Region 1 coordinates to map it to
+    its matching point in Image 2 coordinates.
 
     Note this only works correctly under OpenCV v2. Under v3, the function 'orb.detectAndCompute()'
     does not work properly for Python - it incorrectly raises an exception. This is a widely known
@@ -25,16 +25,16 @@ class FeatureMatcher:
     _MAX_MATCHES = 200
 
     def __init__(self, img1, img2):
-        """ Feature Matching between two images. If rectangular regions are provided, the routine will
-        only consider features in those regions. """
         self._detector = FeatureDetector()
 
         self.img1 = img1
         self.img2 = img2
 
+    # -------- CONFIGURATION -------------------
     def set_detector(self, method, adaptation=""):
         self._detector = FeatureDetector(method, adaptation)
 
+    # -------- FUNCTIONALITY -------------------
     def match(self, translation_only=False):
         matches = self._find_matches()
         homography = MatchHomographyCalculator()
@@ -85,8 +85,7 @@ class FeatureMatcher:
 
     def _brute_force_match(self, method, descriptors_1, descriptors_2):
         """ For two sets of feature descriptors generated from 2 images, attempt to find all the matches,
-        i.e. find features that occur in both images.
-        """
+        i.e. find features that occur in both images. """
         # TODO: Try out a FLANN based matcher
         if len(descriptors_1) == 0 or len(descriptors_2) == 0:
             return []
@@ -103,29 +102,3 @@ class FeatureMatcher:
     def _raise_insufficient_matches_exception(self):
         message = "Could not find the required minimum number of matches ({})!".format(self._MIN_MATCHES)
         raise FeatureMatchException(message)
-
-
-class BoundedFeatureMatcher(FeatureMatcher):
-    def __init__(self, img1, img2, img1_rect, img2_rect):
-        """ Allows user to specify sub regions of the two images. When performing the matching operations,
-        only features in these regions will be considered. """
-        FeatureMatcher.__init__(self, img1, img2)
-
-        if img1_rect is None:
-            img1_rect = img1.bounds()
-
-        if img2_rect is None:
-            img2_rect = img2.bounds()
-
-        self.img1 = img1.crop(img1_rect)
-        self.img2 = img2.crop(img2_rect)
-
-        self.img1_offset = img1_rect.top_left()
-        self.img2_offset = img2_rect.top_left()
-
-    def _matches_from_raw(self, raw_matches, keypoints1, keypoints2, method):
-        matches = SingleFeatureMatch.matches_from_raw(raw_matches, keypoints1, keypoints2, method)
-        for match in matches:
-            match.set_offsets(self.img1_offset, self.img2_offset)
-
-        return matches
---
+++ dls_imagematch/match/feature/matcher_bounded.py
@@ -0,0 +1,30 @@
+from __future__ import division
+
+from .matcher import FeatureMatcher
+from ._single_match import SingleFeatureMatch
+
+
+class BoundedFeatureMatcher(FeatureMatcher):
+    """ Specialization of feature matcher which allows user to specify sub regions of the two images.
+    When performing the matching operations, only features in these regions will be considered. """
+    def __init__(self, img1, img2, img1_rect, img2_rect):
+        FeatureMatcher.__init__(self, img1, img2)
+
+        if img1_rect is None:
+            img1_rect = img1.bounds()
+
+        if img2_rect is None:
+            img2_rect = img2.bounds()
+
+        self.img1 = img1.crop(img1_rect)
+        self.img2 = img2.crop(img2_rect)
+
+        self.img1_offset = img1_rect.top_left()
+        self.img2_offset = img2_rect.top_left()
+
+    def _matches_from_raw(self, raw_matches, keypoints1, keypoints2, method):
+        matches = SingleFeatureMatch.matches_from_raw(raw_matches, keypoints1, keypoints2, method)
+        for match in matches:
+            match.set_offsets(self.img1_offset, self.img2_offset)
+
+        return matches
