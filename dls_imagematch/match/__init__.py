58043305a317231ed32ba997355279009166fe05
Kris Ward <kristopher.ward@tessella.com>
2016-07-26 15:22:37 +0100

    Added missing import statement

--- dls_imagematch/match/__init__.py
+++ dls_imagematch/match/__init__.py
@@ -3,6 +3,7 @@ from dls_imagematch.match.crystal.matcher import CrystalMatcher
 from dls_imagematch.match.feature import FeatureMatcher, FeatureDetector, FeatureMatchException
 from dls_imagematch.match.region.match_consensus import RegionConsensusMatcher
 from dls_imagematch.match.region.match_region import RegionMatcher
+from dls_imagematch.match.transform import *
 from .aligned_images import AlignedImages
 from .metric_overlap import OverlapMetric
 from .overlay import Overlayer
