04e8d00a9513f5847e8bf0faf68821e6e67841e6
Kris Ward <kristopher.ward@tessella.com>
2016-07-27 10:30:34 +0100

    Deleted old region matching related code as it is no longer used

--- dls_imagematch/gui/__init__.py
+++ dls_imagematch/gui/__init__.py
@@ -2,11 +2,8 @@ from .crystal.match_control import CrystalMatchControl
 from .crystal._point_select_dialog import PointSelectDialog
 from .image_frame import ImageFrame
 from .image_select import ImageSelector
-from .match_consensus_control import ConsensusMatchControl
 from .match_feature_control import FeatureMatchControl
-from .match_region_control import RegionMatchControl
 from .progress_dialog import ProgressDialog
-from .well_select import WellSelector
 from .well_select2 import WellSelector2
 
 
--- dls_imagematch/match/__init__.py
+++ dls_imagematch/match/__init__.py
@@ -1,8 +1,6 @@
 from dls_imagematch.match.crystal.match_results import CrystalMatchResults
 from dls_imagematch.match.crystal.matcher import CrystalMatcher
 from dls_imagematch.match.feature import FeatureMatcher, FeatureDetector, FeatureMatchException
-from dls_imagematch.match.region.match_consensus import RegionConsensusMatcher
-from dls_imagematch.match.region.match_region import RegionMatcher
 from dls_imagematch.match.transform import *
 from .aligned_images import AlignedImages
 from .metric_overlap import OverlapMetric
