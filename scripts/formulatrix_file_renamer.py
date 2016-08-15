f5d34894c44b27f1a8ebc72715de406c6a38b14e
Kris Ward <kristopher.ward@tessella.com>
2016-08-15 11:36:33 +0100

    Fixed formulatrix renamer

--- scripts/formulatrix_file_renamer.py
+++ scripts/formulatrix_file_renamer.py
@@ -16,13 +16,15 @@ def get_sub_dirs(dir, startswith="", endswith=""):
     sub_dirs = [p for p in paths if os.path.isdir(p)]
     return sub_dirs
 
+
 def get_files_with_dir(dir):
     """ Return a list of all files (full path) in the directory. """
     paths = [os.path.join(dir,o) for o in os.listdir(dir)]
     files = [p for p in paths if os.path.isfile(p)]
 
     return files
-    
+
+
 def get_files(dir):
     """ Return a list of all files (full path) in the directory. """
     files = [o for o in os.listdir(dir) if os.path.isfile(os.path.join(dir,o))]
@@ -30,43 +32,47 @@ def get_files(dir):
     return files
 
 
-TARGET_DIRECTORY = "."
+def name_generator():
+    forward = False
+    for row in "ABCDEFGH":
+        forward ^= True
+        cols = range(1, 13) if forward else range(12, 0, -1)
+        for col in cols:
+            name = str(row) + str(col).zfill(2)
+            yield name
 
-forward = False
-for row in "ABCDEFGH":
-    forward ^= True
-    cols = range(1, 13) if forward else range(12, 0, -1)
-    for col in cols:
-        name = str(row) + str(col).zfill(2)
-        print(name)
-    
-# # Takes a set of directories which each contain an image series from the formulatrix
-# dir_list = get_sub_dirs(TARGET_DIRECTORY)
-#
-# # Give files ascending numerical names: 1, 2, 3,... etc
-# for dir in dir_list:
-#     # Delete thumbnail files
-#     files = get_files_with_dir(dir)
-#     for file in files:
-#         if file.endswith("th.jpg") or file.endswith("th..jpg"):
-#             os.remove(file)
-#
-#     # If the folder doesn't contain 96 files, skip it
-#     files = get_files(dir)
-#     if len(files) != 96:
-#         print("Directory {} contains {} files, should be 96. Skipping.".format(dir, len(files)))
-#         continue
-#
-#     files = sorted(files, key=lambda x: int(x[:-4]))
-#     for i, file in enumerate(files):
-#         old_name = dir + "\\" + file
-#         new_name = dir + "\\" + str(i+1).zfill(2) + ".jpg"
-#         os.rename(old_name, new_name)
-#
-#     forward = False
-#     for row in "ABCDEFGH":
-#         forward ^= True
-#         cols = range(1, 13) if forward else range(12, 1, -1)
-#         for col in cols:
-#             name = str(row) + str(col).zfill(2)
-#             print(name)
+
+TARGET_DIRECTORY = "../test-images/Formulatrix/test"
+
+# Takes a set of directories which each contain an image series from the formulatrix
+dir_list = get_sub_dirs(TARGET_DIRECTORY)
+
+# Give files ascending numerical names: 1, 2, 3,... etc
+for directory in dir_list:
+    # Delete thumbnail files
+    files = get_files_with_dir(directory)
+    for file in files:
+        if file.endswith("th.jpg") or file.endswith("th..jpg"):
+            os.remove(file)
+
+    # The folder must contain exactly 96 .jpg files
+    files = get_files(directory)
+    if len(files) != 96:
+        print("Directory {} contains {} files, should be 96. Skipping.".format(directory, len(files)))
+        continue
+    else:
+        bad_extensions = [f[-4:] != ".jpg" for f in files]
+        if any(bad_extensions):
+            print("Directory {} contains file with '{}' extension, "
+                  "should be '.jpg'. Skipping.".format(directory, file[-4:]))
+            continue
+
+    # Rename the files
+    print("Renaming files in directory {}".format(directory))
+
+    names = name_generator()
+    files = sorted(files, key=lambda x: int(x[:-4]))
+    for i, file in enumerate(files):
+        old_name = directory + "\\" + file
+        new_name = directory + "\\" + next(names) + ".jpg"
+        os.rename(old_name, new_name)
