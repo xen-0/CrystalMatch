d9d51a379eaf9ed70e61eae56bdd95598d826cbf
Kris Ward <kristopher.ward@tessella.com>
2016-08-22 13:50:24 +0100

    Renamed variable

--- dls_imagematch/match/feature/detector/detector_sift.py
+++ dls_imagematch/match/feature/detector/detector_sift.py
@@ -7,7 +7,7 @@ from .detector import Detector
 
 class SiftDetector(Detector):
     DEFAULT_N_FEATURES = 500
-    DEFAULT_OCTAVE_LAYERS = 3
+    DEFAULT_N_OCTAVE_LAYERS = 3
     DEFAULT_CONTRAST_THRESHOLD = 0.04
     DEFAULT_EDGE_THRESHOLD = 10
     DEFAULT_SIGMA = 1.6
@@ -16,7 +16,7 @@ class SiftDetector(Detector):
         Detector.__init__(self, DetectorType.SIFT)
 
         self._n_features = self.DEFAULT_N_FEATURES
-        self._octave_layers = self.DEFAULT_OCTAVE_LAYERS
+        self._n_octave_layers = self.DEFAULT_N_OCTAVE_LAYERS
         self._contrast_threshold = self.DEFAULT_CONTRAST_THRESHOLD
         self._edge_threshold = self.DEFAULT_EDGE_THRESHOLD
         self._sigma = self.DEFAULT_SIGMA
@@ -31,7 +31,7 @@ class SiftDetector(Detector):
     def set_octave_layers(self, value):
         """ The number of layers in each octave. 3 is the value used in D. Lowe paper. The number of octaves
         is computed automatically from the image resolution. """
-        self._octave_layers = int(value)
+        self._n_octave_layers = int(value)
 
     def set_contrast_threshold(self, value):
         """ The contrast threshold used to filter out weak features in semi-uniform (low-contrast) regions.
@@ -53,7 +53,7 @@ class SiftDetector(Detector):
     def _create_detector(self):
         print("Creating SIFT detector")
         detector = cv2.SIFT(nfeatures=self._n_features,
-                            nOctaveLayers=self._octave_layers,
+                            nOctaveLayers=self._n_octave_layers,
                             contrastThreshold=self._contrast_threshold,
                             edgeThreshold=self._edge_threshold,
                             sigma=self._sigma)
