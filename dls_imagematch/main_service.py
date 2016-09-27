7328eb35f847efbb48ecfbe6801a67724bf6ca86
Chris Martin (Tessella) <marcs@tessella.com>
2016-09-27 11:14:39 +0100

    Add an argument parser to the Service, allow users to enter a before image, after image and a list of comma separated x,y co-ordinates for the selected points.  Carries out basic string validation on the points (does not check the validity of the points themselves).

--- dls_imagematch/main_service.py
+++ dls_imagematch/main_service.py
@@ -1,11 +1,11 @@
 import sys
-
+import argparse
+import re
+from dls_util.shape import Point
 from os.path import dirname
 from sys import path
-path.append(dirname(path[0]))
-
-
 from service import CrystalMatchService
+path.append(dirname(path[0]))
 
 # Detect if the program is running from source or has been bundled
 IS_BUNDLED = getattr(sys, 'frozen', False)
@@ -16,15 +16,45 @@ else:
 
 
 def main():
-    dir = "../test-images/Formulatrix/46412/"
-    image1_path = dir + "449/A02.jpg"
-    image2_path = dir + "584/A02.jpg"
+    parser = get_argument_parser()
+    args = parser.parse_args()
 
-    from dls_util.shape import Point
-    selected = [Point(1068, 442), Point(1191, 1415)]
+    selected_points = parse_selected_points_from_args(args)
 
     service = CrystalMatchService(CONFIG_DIR)
-    service.perform_match(image1_path, image2_path, selected)
+    service.perform_match(args.image_marked.name, args.image_target.name, selected_points)
+
+
+def parse_selected_points_from_args(args):
+    selected_points = []
+    point_expected_format = re.compile("[0-9]+,[0-9]+")
+    for point_string in args.coordinates:
+        point_string = point_string.strip('()')
+        if point_expected_format.fullmatch(point_string) is not None:
+            x, y = map(int, point_string.strip('()').split(','))
+            selected_points.append(Point(x, y))
+        else:
+            print ("WARNING: Selected point with invalid format will be ignored - '" + point_string + "'")
+    return selected_points
+
+
+def get_argument_parser():
+    parser = argparse.ArgumentParser(description="Run Crystal Matching algorithm attempting to translate co-ordinates "
+                                                 "from a marked image to the target image.")
+    parser.add_argument('image_marked',
+                        metavar="marked_image",
+                        type=file,
+                        help='Image file corresponding to the co-ordinates provided.')
+    parser.add_argument('image_target',
+                        metavar="target_image",
+                        type=file,
+                        help='Image file on which to find translated co-ordinates.')
+    parser.add_argument('selected_points',
+                        metavar="x,y",
+                        nargs='+',
+                        help="Comma-separated co-ordinates of selected points to be translated from the marked image "
+                             "to the target image.")
+    return parser
 
 
 if __name__ == '__main__':
