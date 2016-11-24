6acbe844828a00d52df372c028a569b4d94e14ed
Chris Martin (Tessella) <marcs@tessella.com>
2016-11-24 16:20:54 +0000

    Update script - flip the process to compare the images in the second directory to images in the main dir - this allows us to move the files as each set of jobs finishes. Change the way we deal with conflicts (a directory is created and all candidates are added to it) and output a CSV file for each image which contains the ordered error values for the matches of that image.

--- scripts/data_cleansing_image_match.py
+++ scripts/data_cleansing_image_match.py
@@ -1,29 +1,32 @@
 # Uses the alignment stage of the CrystalMatch algorithm to compare images in two directories and attempt to rename
-# images in the second directory which do not match their counterparts in the main directory. Any images for which a
-# match cannot be found at the end of the process will be left in the 'unmatched' directory created in each target
-# directory.
+# images in the second directory which do not match their counterparts in the main directory. Renamed images are moved
+# to a new directory called 'matched' at the root of the second directory, if multiple matches are found a directory
+# is created to contain all the possible matches.  To help fix these conflicts a CSV file will be created for each
+# file which lists the error value for each match found for a specified image.
+# NOTE: The restructured files record the process of the script - it can therefore be stopped and restarted at will.
+import csv
 from multiprocessing import Pool
 from os import listdir, makedirs, rename
-from os.path import join, exists, isdir
+from os.path import join, exists, isdir, splitext
 from dls_imagematch.service.service import CrystalMatchService
 
 # CONFIGURATION
 ######################################################################
 MAIN_DIR = "../test-images/Formulatrix/46532/545"
 TARGET_DIR_LIST = [
-    "../test-images/Formulatrix/46532/548",
-    "../test-images/Formulatrix/46532/551",
-    "../test-images/Formulatrix/46532/554",
-    "../test-images/Formulatrix/46532/557",
-    "../test-images/Formulatrix/46532/560",
-    "../test-images/Formulatrix/46532/563",
-    "../test-images/Formulatrix/46532/629",
+    # "../test-images/Formulatrix/46532/548",
+    # "../test-images/Formulatrix/46532/551",
+    # "../test-images/Formulatrix/46532/554",
+    # "../test-images/Formulatrix/46532/557",
+    # "../test-images/Formulatrix/46532/560",
+    # "../test-images/Formulatrix/46532/563",
+    # "../test-images/Formulatrix/46532/629",
     "../test-images/Formulatrix/46532/635",
-    "../test-images/Formulatrix/46532/644",
+    # "../test-images/Formulatrix/46532/644",
 ]
 CONFIG_DIR = "../config"
 UNMATCHED_DIR_NAME = "unmatched"
-ACCEPTABLE_ERROR_THRESHOLD = 13
+PARALLEL_PROCESSES = None  # Set to None to create one process for each core
 ######################################################################
 
 
@@ -51,130 +54,94 @@ def move_matched_files(parent_dir_1, parent_dir_2, true_file_name, replaced_file
     rename(join(unmatched_dir, replaced_file_name), join(parent_dir_2, true_file_name))
 
 
-def compare_image_directories(target_dir):
-    dir_list_1 = listdir(MAIN_DIR)
-    dir_list_2 = listdir(target_dir)
-    alignment_service = CrystalMatchService(CONFIG_DIR)
-    for image_name in dir_list_1:
-        # Skip non-image files
-        if not validate_image_file(image_name):
-            print "Skipping in src dir: " + image_name
-            continue
-        # Check matching image names first
-        if image_name in dir_list_2:
-            dir_list_2.remove(image_name)
-            image_1 = join(MAIN_DIR, image_name)
-            image_2 = join(target_dir, image_name)
-            results = alignment_service.perform_match(image_1, image_2, [])
-            # noinspection PyProtectedMember
-            if results._alignment_status_code.code != 1:
-                move_to_unmatched(MAIN_DIR, image_name)
-                move_to_unmatched(target_dir, image_name)
-        else:
-            # Move to unmatched for the second pass
-            move_to_unmatched(MAIN_DIR, image_name)
-    for unmatched_file in dir_list_2:
-        if not validate_image_file(unmatched_file):
-            print "Skipping in dst dir: " + unmatched_file
-            continue
-        move_to_unmatched(target_dir, unmatched_file)
-
-    # Second Pass - iterate over each image in unmatched attempting to find a match.
-    unmatched_list_1 = listdir(get_unmatched_dir(MAIN_DIR))
-    unmatched_list_2 = listdir(get_unmatched_dir(target_dir))
-    for unmatched_img_name in unmatched_list_1:
-        index = 0
-        target_image = join(get_unmatched_dir(MAIN_DIR), unmatched_img_name)
-        err_val = 0
-        while index < len(unmatched_list_2):
-            potential_img_name = unmatched_list_2[index]
-            candidate_image = join(get_unmatched_dir(target_dir), potential_img_name)
-            results = alignment_service.perform_match(target_image, candidate_image, [])
-            # noinspection PyProtectedMember
-            err_val = results._alignment_error
-            if err_val < ACCEPTABLE_ERROR_THRESHOLD:
-                break
-            index += 1
-        if index < len(unmatched_list_2):
-            print 'Match found "' + unmatched_img_name + '" -> "' + potential_img_name + \
-                  '" (err: ' + str(err_val) + ')'
-            move_matched_files(MAIN_DIR, target_dir, unmatched_img_name, potential_img_name)
-            unmatched_list_2.remove(potential_img_name)
-        else:
-            print 'No match found for "' + unmatched_img_name + '"'
-
-    # Finally restore images in the main unmatched directory as they may be required for next comparison
-    print "Relocating files from '" + get_unmatched_dir(MAIN_DIR) + "'..."
-    for file_name in listdir(get_unmatched_dir(MAIN_DIR)):
-        rename(join(get_unmatched_dir(MAIN_DIR), file_name), join(MAIN_DIR, file_name))
-
-
-# class Worker:
-#     def __init__(self):
-#
-
 # noinspection PyProtectedMember
-def something(bundle):
-    candidate_name, image1, image2 = bundle
+def run_match(bundle):
+    candidate_name, target_image, candidate_image = bundle
     service = CrystalMatchService(CONFIG_DIR)
-    result = service.perform_match(image1, image2, [])
+    result = service.perform_match(target_image, candidate_image, [])
     return candidate_name, result._alignment_status_code.code, result._alignment_error
 
 
 # noinspection PyProtectedMember
 def exhaustive_compare_image_directories(target_dir):
-    dir_list_1 = []
-    dir_list_2 = []
+    main_dir_list = []
+    target_dir_list = []
 
     # Get file lists
     for file_name in listdir(MAIN_DIR):
         if validate_image_file(file_name):
-            dir_list_1.append(file_name)
+            main_dir_list.append(file_name)
     for file_name in listdir(target_dir):
         if validate_image_file(file_name):
-            dir_list_2.append(file_name)
+            target_dir_list.append(file_name)
+
+    # Ensure the matched directory exists
+    cpy_dir = join(target_dir, "matched")
+    if not exists(cpy_dir):
+        makedirs(cpy_dir)
 
     # Compare lists
-    match_list = []
-    for original_image in dir_list_1:
+    for target_img_name in target_dir_list:
         jobs = []
-        for target_image in dir_list_2:
-            jobs.append((target_image, join(MAIN_DIR, original_image), join(target_dir, target_image)))
-            break
+        for orig_img_name in main_dir_list:
+            jobs.append((orig_img_name, join(target_dir, target_img_name), join(MAIN_DIR, orig_img_name)))
 
         # Set up a worker pool for this job set
-        worker_pool = Pool()
-        results = worker_pool.imap(something, jobs)
+        worker_pool = Pool(PARALLEL_PROCESSES)
+        results = worker_pool.imap(run_match, jobs)
         worker_pool.close()
         worker_pool.join()
 
         # interpret the results.
-        best_match = None
-        min_err = 9999
+        csv_data = []
         for i in range(results._length):
-            candidate_name, state_code, error = results.next()
-            if state_code == 1 and error < min_err:
-                best_match = candidate_name
-                min_err = error
-        if best_match is not None:
-            print "Suggested match: '" + original_image + "' -> '" + str(best_match) + \
-                          "' (err: " + str(min_err) + ")"
-            match_list.append((original_image, best_match, min_err))
+            candidate_name, state_code, error_value = results.next()
+            # TODO: Output match data to csv file
+            if state_code == 1:
+                csv_data.append([candidate_name, error_value])
+        # TODO: print to console
+        if len(csv_data) > 0:
+            csv_data = sorted(csv_data, key=lambda x: x[1])
+            best_match = csv_data[0][0]
+            old_file_path = join(target_dir, target_img_name)
+            new_img_path = join(cpy_dir, best_match)
+            new_img_dir = splitext(new_img_path)[0]
+            if exists(new_img_path):
+                # Create a dir and copy both matches into it - add a suffix to each file name
+                makedirs(new_img_dir)
+                existing_file = get_unique_filename(new_img_dir, best_match)
+                rename(new_img_path, existing_file)
+                rename(splitext(new_img_path)[0] + '.csv', splitext(existing_file)[0] + '.csv')
+                new_image_path = get_unique_filename(new_img_dir, best_match)
+                rename(old_file_path, new_image_path)
+            elif exists(new_img_dir) and isdir(new_img_dir):
+                # Copy the image to the directory with a suffix
+                new_image_path = get_unique_filename(new_img_dir, best_match)
+                rename(old_file_path, new_image_path)
+            else:
+                # Copy the image to the matched directory.
+                new_image_path = join(cpy_dir, best_match)
+                rename(old_file_path, new_image_path)
+            # Create the csv file alongside the moved file.
+            write_csv_file_for_matches(new_image_path, csv_data)
         else:
-            print "No match found: '" + original_image + "'"
+            print "No match found: '" + target_img_name + "'"
 
-    # Move files
-    cpy_dir = join(target_dir, "matched")
-    if not exists(cpy_dir):
-        makedirs(cpy_dir)
-    for m in match_list:
-        original_image, best_match, min_err = m
-        old_file = join(target_dir, best_match)
-        new_file = join(cpy_dir, original_image)
-        if exists(old_file):
-            rename(old_file, new_file)
-        else:
-            print "Conflicted file! '" + old_file + "' could not be copied to '" + new_file + "'"
+
+def write_csv_file_for_matches(image_file_path, csv_data):
+    image_file_path = splitext(image_file_path)[0] + '.csv'
+    with open(image_file_path, 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(csv_data)
+
+
+def get_unique_filename(directory, filename):
+    i = 0
+    split_path = splitext(join(directory, filename))
+    while exists(split_path[0] + "_" + str(i) + split_path[1]):
+        i += 1
+    img_dir_path = split_path[0] + "_" + str(i) + split_path[1]
+    return img_dir_path
 
 
 if __name__ == '__main__':
