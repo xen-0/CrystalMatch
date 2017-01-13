464a371079ff81c8a4eb1e374dbbebe36ecc5ab4
Chris Martin (Tessella) <marcs@tessella.com>
2017-01-13 16:51:27 +0000

    Merge test-image directories

--- dls_focusstack/focus_config.py
+++ dls_focusstack/focus_config.py
@@ -13,7 +13,7 @@ class FocusConfig(Config):
         self.align_method = add(EnumConfigItem, "Alignment Method", default="SURF", extra_arg=methods)
         self.kernel_size = add(IntConfigItem, "Laplacian Kernel Size", default=5, extra_arg='px')
         self.blur_radius = add(IntConfigItem, "Laplacian Blur Radius", default=5, extra_arg='px')
-        self.input_dir = add(DirectoryConfigItem, "Input Directory", default="../test-images-focus-stack/VMXI-AA005-G07-1-R0DRP1")
+        self.input_dir = add(DirectoryConfigItem, "Input Directory", default="../test-images/Focus Stacking/VMXI-AA005-G07-1-R0DRP1")
         self.output_dir = add(DirectoryConfigItem, "Output Directory", default="../test-output/focus/")
 
         self.initialize_from_file()
