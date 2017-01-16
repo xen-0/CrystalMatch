a8004ceddcd94328cf5fb93162c6c72b13b340da
Chris Martin (Tessella) <marcs@tessella.com>
2017-01-16 14:55:53 +0000

    Remove alignment step from the focus stacking process - camera stability should be constant and correcting for the perspective distortion can be done using set increments.

--- dls_focusstack/focus/focus_stack.py
+++ dls_focusstack/focus/focus_stack.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 
-from dls_imagematch.feature import FeatureMatcher
 from dls_util.imaging import Image
 
 
@@ -10,16 +9,10 @@ class FocusStack:
         self._images = images
         self._config = config
 
-    def composite(self, with_align=False):
+    def composite(self):
         """ Finds the points of best focus in all images and produces a merged result """
         cfg = self._config
-
-        if with_align:
-            method = cfg.align_method.value()
-            out_dir = cfg.output_dir.value()
-            images = self._align_images(self._images, method, out_dir)
-        else:
-            images = self._images
+        images = self._images
 
         kernel_size = cfg.kernel_size.value()
         blur_radius = cfg.blur_radius.value()
@@ -28,31 +21,6 @@ class FocusStack:
         focused_image = self._determine_focused_pixels(images, laplacians)
 
         return focused_image
-
-    @staticmethod
-    def _align_images(images, method, out_dir):
-        aligned_images = []
-
-        # TODO - Frame-by-frame alignment where each frame is aligned with the previous one. This will mean
-        # TODO - that the transform for a frame needs to be the product of all transforms that proceeded it
-
-        image1 = images[0]
-        for i in range(1, len(images)):
-            print("Aligning image {}/{}".format(i+1, len(images)))
-            #image1 = images[i-1]
-            image2 = images[i]
-            matcher = FeatureMatcher(image1, image2)
-            matcher.set_detector(method)
-            matcher.set_transform_method("Homography")
-
-            transform = matcher.match().transform()
-            transformed_image = transform.inverse_transform_image(image2, image2.size())
-            transformed_image.save("{}aligned{}.png".format(out_dir, i))
-            #transformed_image.popup()
-
-            aligned_images.append(transformed_image)
-
-        return aligned_images
 
     @staticmethod
     def _compute_laplacians(images, kernel_size, blur_size):
