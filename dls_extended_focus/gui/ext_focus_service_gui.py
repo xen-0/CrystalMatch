e5e4764d2b290ad8eb17cf62be37199d20347416
Chris Martin (Tessella) <marcs@tessella.com>
2017-03-30 12:33:07 +0100

    Add a request form to the main GUI.

--- dls_extended_focus/gui/ext_focus_service_gui.py
+++ dls_extended_focus/gui/ext_focus_service_gui.py
@@ -1,5 +1,5 @@
-from Tkconstants import X, DISABLED, NORMAL
-from Tkinter import Tk, Button
+from Tkconstants import X, DISABLED, NORMAL, SUNKEN, Y, LEFT, W
+from Tkinter import Tk, Button, LabelFrame, Entry, StringVar, Label
 
 
 class ExtendedFocusServiceGUI(Tk):
@@ -11,13 +11,46 @@ class ExtendedFocusServiceGUI(Tk):
         Tk.__init__(self, "Extended Focus Service", None, "ExtendedFocusServiceGUI")
         self._service = ext_focus_service
         self.wm_title("Extended Focus Service")
-        self.button_start = Button(self, text="Start", command=self._start_service)
+
+        # Controls Frame
+        self.frm_controls = LabelFrame(self, text="Controls", borderwidth=1, relief=SUNKEN)
+        self.button_start = Button(self.frm_controls, text="Start", command=self._start_service)
         self.button_start.pack(fill=X, padx=2, pady=2)
-        self.button_stop = Button(self, text="Stop", command=self._stop_service)
+        self.button_stop = Button(self.frm_controls, text="Stop", command=self._stop_service)
         self.button_stop.pack(fill=X, padx=2, pady=2)
-        self.button_force_stop = Button(self, text="Force Stop", command=self._force_stop_service)
-        self.button_force_stop.pack(fill=X, padx=2, pady=2)
+        self.button_force_stop = Button(self.frm_controls, text="Force Stop", command=self._force_stop_service)
+        self.button_force_stop.pack(fill=Y, padx=2, pady=2)
+        self.frm_controls.pack(fill=Y, side=LEFT, padx=2, pady=2)
+
+        # Test Request Frame
+        self.frm_request = LabelFrame(self, text="Send Request", borderwidth=1, relief=SUNKEN)
+
+        self.val_job_id = self._create_field(self.frm_request, "Job ID", "TEST_00000")
+        self.val_target_dir = self._create_field(self.frm_request, "Target Dir", "")
+        self.val_target_dir = self._create_field(self.frm_request, "Output Path", "")
+
+        self.button_send_req = Button(self.frm_request, text="Send Request", command=self._send_request())
+        self.button_send_req.pack(fill=X, padx=2, pady=2)
+        self.frm_request.pack(fill=X, padx=2, pady=2)
+
         self._start_service()
+
+    @staticmethod
+    def _create_field(parent, label_txt, default_val):
+        """
+        Create a a label and field pair returning the StringVar object which will be tied to it.
+        :param parent: The parent element to which this field will be added.
+        :param label_txt: Text for label.
+        :param default_val: Default value for text box.
+        :return: The StringVal for the text box.
+        """
+        string_var = StringVar()
+        string_var.set(default_val)
+        label = Label(parent, text=label_txt, anchor=W)
+        label.pack(fill=X, padx=2, pady=2)
+        entry_box = Entry(parent, textvariable=string_var)
+        entry_box.pack(fill=X, padx=2, pady=2)
+        return string_var
 
     def destroy(self):
         if self._service.is_connected():
@@ -25,6 +58,10 @@ class ExtendedFocusServiceGUI(Tk):
         Tk.destroy(self)
 
     # Button functions
+
+    def _send_request(self):
+        # TODO: send request
+        pass
 
     def _start_service(self):
         self.button_start["state"] = DISABLED
