acc5aa7d71cc8d5236a02749c85a0148b24ea909
Chris Martin (Tessella) <marcs@tessella.com>
2018-04-12 11:42:04 +0100

    Moving to Linux platform for development

--- scripts/GDA_log_extraction.py
+++ scripts/GDA_log_extraction.py
@@ -1,11 +1,21 @@
 # Parse a GDA log file to find instances of CrystalMatch Commands and create a datafile from the images.
 import gzip
 
-filename = "/dls/science/groups/das/Archive/i02-2/logs/gda-server-20180130.log.gz"
-fp = None
+from os import listdir, remove
+from os.path import join
 
-if filename.endswith(".gz"):
-    fp = gzip.open(filename)
-else:
-    fp = open(filename, "r")
+directory = join(".", "gda_logs")
+listdir(directory)
+files = [f for f in listdir(directory) if f.endswith('.txt') or f.endswith(".log.gz")]
 
+for filename in files:
+    filename = join(directory, filename)
+    fp = None
+    if filename.endswith(".gz"):
+        fp = gzip.open(filename)
+    else:
+        fp = open(filename, "r")
+    for line in fp:
+        print(line)
+    fp.close()
+    # remove(filename)
