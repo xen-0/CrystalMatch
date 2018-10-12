e0e20d2a7af29321992465f5041ea008cc6a68e6
rqq82173 <urszula.neuman@gmail.com>
2018-10-12 15:12:17 +0100

    setup, Makefile, requirements

---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+
+# these lines allow the version to be specified in Makefile.private
+import os
+version = os.environ.get("MODULEVER", "0.0")
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name = 'CrystalMatch',
+    version = '0.0.9',
+    description = 'Python Toolkit for Matching Points on Formulatrix Images to Points on Beamline Images',
+    long_description=long_description,
+    url='https://github.com/DiamondLightSource/CrystalMatch',
+    author = 'Urszula Neuman',
+    author_email = 'urszula.neuman@dimaond.ac.uk',
+    license='Apache License 2.0',
+
+    packages=[x for x in setuptools.find_packages() if x.startswith("CrystalMatch")],
+    include_package_data = True,
+
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    entry_points={'console_scripts': ['CrystalMatch = CrystalMatch.CrystalMatch.dls_imagematch:main_service']},  # this makes a script
+
+    install_requires=['numpy==1.11.1', 'pygelf', 'scipy'],
+
+    tests_require=['mock', 'nose_parameterized'],
+
+    zip_safe = False
+)
